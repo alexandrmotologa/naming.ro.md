6b99134be849c83cbf3969c81e8ad02ec183e0b8
alexandrmotologa <al.motologa@gmail.com>
2020-07-23 21:50:16 +0300

    explicat greseala, scris 3 variabile, functia print, verificat prin PEP8 online

---
+++ naming_home_work.py
@@ -0,0 +1,13 @@
+students_firstname = "John"
+students_lastname = "Doe"
+# denumirea compusa trebuie sa fie separata vizual
+# ( students_lastname, studentsLastname, StudentsLastname )
+students_age = 19
+students_country_origin = "Netherlands"
+sutdents_hometown = "Haarlem"
+
+print("Prenumele studentului: ", students_firstname)
+print("Numele studentului: ", students_lastname)
+print("Varsta studentului: ", students_age)
+print("Tara de provenienta: ", students_country_origin)
+print("Orasul de bastina: ", sutdents_hometown)
